8c123ae8d6aa0e4def0f3a1ea1f36d73c0fd73de
Rodrigo Rodriguez (Pragmatismo) <me@rodrigorodriguez.com>
2026-02-05 14:59:57 -0300

    Keep 3rdparty.toml embedded in binary (already using include_str!)

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
 fn main() {
     println!("cargo:rerun-if-changed=../botui/ui/suite/");
+    println!("cargo:rerun-if-changed=3rdparty.toml");
+    println!("cargo:rerun-if-changed=.env.embedded");
 }
