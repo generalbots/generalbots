8caafd5be3a0cf1f5338f533ba89f7a79f8600ca
a404010 <a404010@pragmatismo.com.br>
2026-08-23 15:17:19 +0000

    refactor(suite/minutes): mindfulness quality refactor
    Closes #1119
    
    - root opts into .gb-app opaque shell with lifecycle registration
    - loading/error intentional states; oversized assets split under 450 lines

--- botui/ui/suite/minutes/minutes.js
+++ botui/ui/suite/minutes/minutes.js
@@ -1,4 +1,5 @@
-"use strict";
+"if (window.GBAppLifecycle) GBAppLifecycle.begin("minutes");
+use strict";
 (function() {
     function activateTab(btn) {
         document.querySelectorAll("[data-tab-trigger]").forEach(function(b) {
